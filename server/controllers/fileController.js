3b400e8da3ae38f614a20edf898e465f26cc0058
Andryii <andryii.protasenko@gmial.com>
2022-10-24 21:53:43 +0300

    Uploading files

--- server/controllers/fileController.js
+++ server/controllers/fileController.js
@@ -1,4 +1,6 @@
 const FileService = require("../services/fileService");
+const config = require("config");
+const fs = require("fs");
 const User = require("../models/User");
 const File = require("../models/File");
 const fileService = require("../services/fileService");
@@ -35,10 +37,63 @@ class FileController {
         user: req.user.id,
         parent: req.query.parent,
       });
-      return res.json( files );
+      return res.json(files);
     } catch (e) {
       console.log(e);
       return res.status(500).json({ message: "Can not get files" });
+    }
+  }
+
+  async uploadFile(req, res) {
+    try {
+      const file = req.files.file;
+
+      const parent = await File.findOne({
+        user: req.user.id,
+        _id: req.body.parent,
+      });
+
+      const user = await User.findOne({ _id: req.user.id });
+
+      if (user.usedSpace + file.size > user.dsikSpace) {
+        return res.status(400).json({ massage: "There no space on the disk" });
+      }
+
+      user.usedSpace = user.usedSpace + file.size;
+
+      let path;
+
+      if (parent) {
+        path = `${config.get("filePath")}\\${user._id}\\${parent.path}\\${
+          file.name
+        }`;
+      } else {
+        path = `${config.get("filePath")}\\${user._id}\\${file.name}`;
+      }
+
+      if (fs.existsSync(path)) {
+        return res.status(400).json({ message: "File already exist" });
+      }
+      file.mv(path);
+
+      const type = file.name.split(".").pop();
+
+      const dbFile = new File({
+        name: file.name,
+        type: type,
+        size: file.size,
+        path: parent?.path,
+        parent: parent?._id,
+        user: user._id,
+      });
+
+      await dbFile.save();
+      await user.save();
+
+      res.json(dbFile);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ message: "Upload error" });
     }
   }
 }
--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const consfig = require("config");
 const authRouter = require("./routes/auth.routes");
+const fileUpload = require("express-fileupload");
 const fileRouter = require("./routes/file.routes");
 const corsMiddleware = require("./middleware/cors.middleware");
 const app = express();
 const PORT = consfig.get("serverPort");
 
+app.use(fileUpload({}));
 app.use(corsMiddleware);
 app.use(express.json());
 app.use("/api/auth", authRouter);
